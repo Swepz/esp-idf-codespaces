e6be6b1015014362f9934f1321effc2f74f28ce7
swepz <taza@live.se>
2022-11-20 22:20:29 +0100

    Testing configuration

--- main/hello_world.c
+++ main/hello_world.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "sdkconfig.h"
 
-void app_main(void) {
-  while (1) {
-    printf("Hello world!") vTaskDelay(1000 / portTICK_PERIOD_MS);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+void hello_task(void *pvParameter) {
+  printf("Hello world!\n");
+  for (int i = 3; i >= 0; i--) {
+    printf("Restarting in %d seconds...\n", i);
+    vTaskDelay(1000 / portTICK_RATE_MS);
   }
+  printf("Restarting now.\n");
+  fflush(stdout);
+  esp_restart();
 }
+
+void app_main() { xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL); }
